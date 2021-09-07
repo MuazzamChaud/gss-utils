ec11da470e03c2b272539f5971ad7b24c1f8953e
Rob Barry <rob.barry@ons.gov.uk>
2021-09-07 15:58:07 +0100

    Upgrading rdflib, including pyRDFa as a replacement.

--- gssutils/scrapers/statswales.py
+++ gssutils/scrapers/statswales.py
@@ -5,37 +5,53 @@ import rdflib
 from dateutil.parser import parse
 from rdflib import RDF, URIRef
 from rdflib.namespace import DCTERMS
+from pyRdfa import pyRdfa as rdfa_processor, Options, HostLanguage
 
-import gssutils.metadata.dcat
 from gssutils.metadata import DCAT, GOV
 from gssutils.metadata.dcat import Distribution
 
 
 def scrape(scraper, tree):
-    pageGraph = rdflib.Graph()
     page = StringIO(scraper.session.get(scraper.uri).text)
-    pageGraph.parse(page, format="html")
+    pageGraph = _parse_rdfa_to_graph(page)
+    # pageGraph.parse(page, format="html")
     dataset = pageGraph.value(predicate=RDF.type, object=DCAT.Dataset, any=False)
     scraper.dataset.title = pageGraph.value(dataset, DCTERMS.title).value.strip()
-    scraper.dataset.comment = pageGraph.value(dataset, DCTERMS.description).value.strip()
+    scraper.dataset.comment = pageGraph.value(
+        dataset, DCTERMS.description
+    ).value.strip()
     license = str(pageGraph.value(dataset, DCTERMS.license))
-    if license == 'http://www.nationalarchives.gov.uk/doc/open-government-licence':
-        scraper.dataset.license = 'http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/'
+    if license == "http://www.nationalarchives.gov.uk/doc/open-government-licence":
+        scraper.dataset.license = (
+            "http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"
+        )
     else:
         scraper.dataset.license = license
     publisher = pageGraph.value(dataset, DCTERMS.publisher)
-    if publisher == URIRef('http://www.gov.wales/'):
-        scraper.dataset.publisher = GOV['welsh-government']
+    if publisher == URIRef("http://www.gov.wales/"):
+        scraper.dataset.publisher = GOV["welsh-government"]
     else:
         scraper.dataset.publisher = publisher
-    scraper.dataset.issued = parse(pageGraph.value(dataset, DCTERMS.created), dayfirst=True).date()
-    scraper.dataset.modified = parse(pageGraph.value(dataset, DCTERMS.modified), dayfirst=True).date()
+    scraper.dataset.issued = parse(
+        pageGraph.value(dataset, DCTERMS.created), dayfirst=True
+    ).date()
+    scraper.dataset.modified = parse(
+        pageGraph.value(dataset, DCTERMS.modified), dayfirst=True
+    ).date()
     for pageDist in pageGraph.subjects(RDF.type, DCAT.Distribution):
         dist = Distribution(scraper)
         dist.title = pageGraph.value(pageDist, DCTERMS.title).value.strip()
         # Access URLs seem to have spaces in their query strings
         url_parts = urlparse(pageGraph.value(pageDist, DCAT.accessURL))
-        dist.downloadURL = url_parts._replace(query=url_parts.query.replace(' ', '+')).geturl()
+        dist.downloadURL = url_parts._replace(
+            query=url_parts.query.replace(" ", "+")
+        ).geturl()
         dist.mediaType = pageGraph.value(pageDist, DCAT.mediaType).value.strip()
         scraper.distributions.append(dist)
 
+
+def _parse_rdfa_to_graph(page: StringIO) -> rdflib.Graph:
+    options = Options()
+    options.host_language = HostLanguage.html5
+    processor = rdfa_processor(options)
+    return processor.graph_from_source(page)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gssutils",
-    version_format='{tag}.dev{commitcount}+{gitsha}',
+    version_format="{tag}.dev{commitcount}+{gitsha}",
     author="Alex Tucker",
     author_email="alex@floop.org.uk",
     description="Common functions used by GSS data transformations",
@@ -13,32 +13,37 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/GSS-Cogs/gss-utils",
     packages=setuptools.find_packages(),
-    setup_requires=['better-setuptools-git-version'],
-    install_requires=['requests',
-                      'python_dateutil',
-                      'CacheControl',
-                      'lockfile',
-                      'databaker @ git+git://github.com/GSS-Cogs/databaker.git@2ddb86121eb563e3a37ee1106529d8b1da197e31#egg=databaker',
-                      'ipython',
-                      'jinja2',
-                      'pandas',
-                      'html2text',
-                      'rdflib==4.2.2',
-                      'rdflib-jsonld',
-                      'lxml',
-                      'unidecode',
-                      'argparse',
-                      'wheel',
-                      'uritemplate',
-                      'backoff',
-                      'vcrpy'],
-    tests_require=['behave', 'parse', 'nose', 'vcrpy', 'docker'],
+    setup_requires=["better-setuptools-git-version"],
+    install_requires=[
+        "requests",
+        "python_dateutil",
+        "CacheControl",
+        "lockfile",
+        "databaker @ git+git://github.com/GSS-Cogs/databaker.git@2ddb86121eb563e3a37ee1106529d8b1da197e31#egg=databaker",
+        "ipython",
+        "jinja2",
+        "pandas",
+        "html2text",
+        "rdflib",
+        "rdflib-jsonld",
+        "lxml",
+        "unidecode",
+        "argparse",
+        "wheel",
+        "uritemplate",
+        "backoff",
+        "vcrpy",
+        "pyRdfa3",
+    ],
+    tests_require=["behave", "parse", "nose", "vcrpy", "docker"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     entry_points={
-        'console_scripts': ['codelist-manager=gssutils.codelistmanager.main:codelist_manager']
-    }
+        "console_scripts": [
+            "codelist-manager=gssutils.codelistmanager.main:codelist_manager"
+        ]
+    },
 )
